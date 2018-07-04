a763e70a4ea4c0d4bcdec2e4df7bf08719eb0b49
Steven Shen <steeve.shen@gmail.com>
2018-07-04 16:03:24 -0400

    testing store

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,9 +5,23 @@ class TurtleDB {
   }
 
   createDB(dbName) {
-    return idb.open('turtleDB', 1, upgradeDB => {
-      upgradeDB.createObjectStore('todos', { keyPath: 'id' });
+    //idb API returns a promise from open()
+    return idb.open(dbName, 1);//, upgradeDB => {
+      // upgradeDB.createObjectStore('todos', { keyPath: 'id' });
       //example - var objectStore = db.createObjectStore("toDoList", { keyPath: "taskTitle" });
+  }
+
+  createStore(storeName) {
+    //IDB API - promise resolves to IDBDatabase object
+    this.dbPromise.then(db => {
+      console.log(db);
+      this.dbPromise = db.open(db.name, db.version + 1, upgradeDB => {
+        upgradeDB.createObjectStore(storeName, { keyPath: 'id' });
+      });
+
+      //try using transaction to add object store
+      // let transaction = db.transaction([], 'versionchange');
+      // let objectStore = transaction.createObjectStore(storeName);
     });
   }
 
@@ -37,9 +51,56 @@ class TurtleDB {
 }
 
 const db = new TurtleDB('turtleDB');
-let doc = {
-    id: 123456,
-    todo: "Finish Project"
+db.createStore('todos');
+// let doc = {
+//     id: 123456,
+//     todo: "Finish Project"
+// };
+// db.create(doc);
+// db.delete(123456);
+
+
+
+var dbName = "sampleDB";
+var dbVersion = 2;
+var request = indexedDB.open(dbName, dbVersion);
+
+request.onupgradeneeded = function(e) {
+  var db = request.result;
+  if (e.oldVersion < 1) {
+    db.createObjectStore("store1");
+  }
+
+  if (e.oldVersion < 2) {
+    db.deleteObjectStore("store1");
+    db.createObjectStore("store2");
+  }
+
+  // etc. for version < 3, 4...
 };
-db.create(doc);
-db.delete(123456);
+
+
+
+
+// function UpgradeDB(db, oldVersion, transaction) {
+//   this._db = db;
+//   this.oldVersion = oldVersion;
+//   this.transaction = new Transaction(transaction);
+// }
+//
+// open: function(name, version, upgradeCallback) {
+//   var p = promisifyRequestCall(indexedDB, 'open', [name, version]);
+//   var request = p.request;
+//
+//   if (request) {
+//     request.onupgradeneeded = function(event) {
+//       if (upgradeCallback) {
+//         upgradeCallback(new UpgradeDB(request.result, event.oldVersion, request.transaction));
+//       }
+//     };
+//   }
+//
+//   return p.then(function(db) {
+//     return new DB(db);
+//   });
+// },
