b48fd49987f91e9b1282943d181c75d211d77cd0
Max Appleton <maxiappleton@gmail.com>
2018-07-04 22:30:34 -0500

    smaller changes

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,34 +1,55 @@
 import React, { Component } from 'react';
 
-export default class Test extends React.Component {
+export default class Test extends Component {
   render() {
     return (
-      <div>
-        <div className="container">
-          <h2>Insert Documents</h2>
+      <div className="container">
+        <h4>DB Name: <span id="db-name"></span></h4>
+        <button className="btn btn-danger" id="delete-db">Delete This Database</button>
 
-          <form className="form-inline" id="insert-form">
-            <div className="form-group">
-              <input className="form-control" type="text" name="title" placeholder="Title"/>
-              <input className="form-control" type="text" name="id" placeholder="Id"/>
-              <input className="btn btn-primary" type="submit" value="Add"/>
-            </div>
-          </form>
+        <h2>(CREATE) Insert A Document</h2>
+        <form className="form-inline" id="insert-form">
+          <div className="form-group">
+            <input className="form-control" type="text" name="id" placeholder="Id" />
+            <input className="form-control" type="text" name="title" placeholder="Title" />
+            <input className="btn btn-primary" type="submit" value="Add" />
+          </div>
+        </form>
 
-          <h2>Console Log All Docs</h2>
-          <button className="btn btn-primary" id="all-docs">Console Log All Docs</button>
+        <h2>(READ) Fetch A Document By Id</h2>
+        <form className="form-inline" id="fetch-form">
+          <div className="form-group">
+            <input className="form-control" type="text" name="id" placeholder="Id" />
+            <input className="btn btn-primary" type="submit" value="Fetch" />
+          </div>
+        </form>
 
-          <h2>Fetch A Document By Id</h2>
-          <form className="form-inline" id="fetch-form">
-            <div className="form-group">
-              <input className="form-control" type="text" name="id" placeholder="Id"/>
-              <input className="btn btn-primary" type="submit" value="Fetch"/>
-            </div>
-          </form>
+        <h2>UPDATE A Document</h2>
+        <form className="form-inline" id="update-form">
+          <div className="form-group">
+            <input className="form-control" type="text" name="id" placeholder="Id" />
+            <input className="form-control" type="text" name="title" placeholder="New Title" />
+            <input className="btn btn-primary" type="submit" value="Update" />
+          </div>
+        </form>
 
-          <h2>All Documents</h2>
-          <ul id="docs" className="list-group"></ul>
-        </div>
+        <h2>DELETE A Document</h2>
+        <form className="form-inline" id="delete-form">
+          <div className="form-group">
+            <input className="form-control" type="text" name="id" placeholder="Id" />
+            <input className="btn btn-primary" type="submit" value="Delete" />
+          </div>
+        </form>
+
+        <hr />
+
+        <h2>Console Log All Docs</h2>
+        <button className="btn btn-primary" id="all-docs">Console Log All Docs</button>
+
+        <hr />
+
+        <h2>All Documents</h2>
+        <ul id="docs" className="list-group"></ul>
       </div>
     )
   }
