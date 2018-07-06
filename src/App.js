8f180eef86769fef8a844cd5a9c1e5e34e8fa811
Steven Shen <steeve.shen@gmail.com>
2018-07-05 23:04:03 -0400

    removed turtleDB functionalities

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import About from './components/About'
 import Header from './components/Header'
-import Test from './components/Test'
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <Header/>
-        <About/> */}
-        <Test />
+        <Header/>
+        <About/>
       </div>
     );
   }
